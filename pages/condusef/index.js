5064b6a5e78c122cc6935e65b15e773d3b5f709f
gomflo <gomflo@gmail.com>
2022-07-26 12:01:27 -0500

    unesacped entity

--- pages/condusef/index.js
+++ pages/condusef/index.js
@@ -393,9 +393,7 @@ export default function CondusefIndex() {
                   </svg>
                 </Disclosure.Button>
                 <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                  If you're unhappy with your purchase for any reason, email us
-                  within 90 days and we'll refund you in full, no questions
-                  asked.
+                  lorem
                 </Disclosure.Panel>
               </>
             )}
